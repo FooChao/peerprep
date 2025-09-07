c3c11693efd750f3007f17b0914c3878551d382f
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-09-07 18:07:21 +0800

    Add home page

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,7 +1,29 @@
+import StatisticPage from "../components/home/StatisticPage";
+import WelcomePage from "../components/home/WelcomePage";
+
+
 export default function HomePage() {
+
     return (
-        <div>
-            This is a home page
+        <div className="h-screen flex flex-col">
+
+            <div className="flex justify-between items-center bg-white w-full h-[5%]">
+                <div className="ml-4 text-black">
+                    hamburger
+                </div>
+                <div className="text-black font-bold">
+                    PeerPrep
+                </div>
+                <div className="ml-4 text-black">
+                    Avatar
+                </div>
+            </div>
+
+            <WelcomePage/>
+
+            <StatisticPage/>
+
+
         </div>
     )
 }
