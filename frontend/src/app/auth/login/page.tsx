6925ddf79f8da8dae8c2749e4b821e75a704fa06
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-09-07 17:26:43 +0800

    Add skeleton pages for both login and signup page

---
+++ frontend/src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "@/app/components/auth/LoginForm";
+
+
+export default function LoginPage() {
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <LoginForm />
+        </div>
+    )
+}
