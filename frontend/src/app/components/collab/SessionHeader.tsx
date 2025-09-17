1b4d8e6e8ee2bbada1b738c7699605eec0b2f98a
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-09-17 22:54:16 +0800

    Add header items

--- frontend/src/app/components/collab/SessionHeader.tsx
+++ frontend/src/app/components/collab/SessionHeader.tsx
@@ -1,7 +1,26 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Mic } from "lucide-react";
+import Image from "next/image";
+
 export default function SessionHeader() {
   return (
-    <header className="bg-stone-800 w-full h-15 text-white border-b-2 border-stone-700">
-      Session Header
+    <header
+      className="flex 
+        justify-end
+        items-center 
+        gap-3
+        bg-stone-800 
+        w-full 
+        h-15 
+        text-white 
+        border-b-2 
+        border-stone-700"
+    >
+      <Button>
+        <Mic />
+      </Button>
+      <Button className="bg-red-500 text-black mr-3">Leave Session</Button>
     </header>
   );
 }
