351a8ed7368c1de40176e39ae3df98e0b6e2f8bd
Derrick Wong <142132416+LemonDrew@users.noreply.github.com>
2025-09-17 22:35:10 +0800

    Touch up on question component

--- frontend/src/app/components/collab/QuestionComponent.tsx
+++ frontend/src/app/components/collab/QuestionComponent.tsx
@@ -13,20 +13,30 @@ export default function QuestionComponent() {
       {/* Criteria */}
       <CardHeader>
         <CardTitle className="text-white text-4xl">Two Sum</CardTitle>
-        <div className="flex flex-1 pt-5 items-start gap-2">
+        <div className="flex pt-5 items-start gap-2">
           <Badge className="bg-red-900 text-black">Difficult</Badge>
         </div>
       </CardHeader>
 
-      <CardContent>
+      <CardContent className="flex flex-col flex-1">
         {/* Question Description */}
-        <div className="flex-1"></div>
+        <div className="flex-1 text-white">
+          Description : We, the citizens of Singapore, pledge ourselves as one
+          united people, regardless of race, language or religion, to build a
+          democratic society based on justice and equality so as to achieve
+          happiness, prosperity and progress for our nation.
+        </div>
 
         {/* Examples Section */}
-        <div className="flex-1"></div>
+        <div className="flex-1 mt-5 p-2 bg-black text-white rounded-lg text-sm">
+          Input : Test case 1 Output : Correct answer Explanation : Because it
+          is correct
+        </div>
 
         {/* Constraints Section */}
-        <div className="flex-1"></div>
+        <div className="mt-5 p-2 flex-1 text-white">
+          1 is less than n There will be a total of 1 trillion test cases
+        </div>
       </CardContent>
     </Card>
   );
