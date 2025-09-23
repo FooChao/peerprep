8553a241971305bea14249c59d6feddcce4246f4
FooChao <e1121427@u.nus.edu>
2025-09-23 23:08:36 +0800

    Update user model with emailCanonical to handle edge case where people type in different casing for same email

--- user-service/model/user-model.js
+++ user-service/model/user-model.js
@@ -13,6 +13,19 @@ const UserModelSchema = new Schema({
     required: true,
     unique: true,
   },
+  // email canonical is used to store a lowercase version of the email for case-insensitive searches
+  // this is to prevent duplicate emails with different cases (e.g., foochao01@gmail.com and FOOCHAO01@gmail.com which essentially is the same thing)
+  // source: https://www.pipedrive.com/en/blog/does-capitalization-matter-in-email 
+  // summary: modern email systems treat the local part (before the @) as case-sensitive, but in practice, most email providers do not.
+  emailCanonical: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    default: function() {
+      return this.email.toLowerCase();
+    }
+  },
   password: {
     type: String,
     required: true,
