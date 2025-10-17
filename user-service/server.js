aaba43d8b3c66a6d0554f74f5bdea273f95552a4
FooChao <e1121427@u.nus.edu>
2025-10-18 01:16:52 +0800

    Move env variables to .env files + add .env.sample for places where .env is expected

--- user-service/server.js
+++ user-service/server.js
@@ -4,16 +4,14 @@ import "dotenv/config";
 import { connectToDB } from "./model/repository.js";
 
 const port = process.env.PORT || 3001;
-const USER_SERVICE_BASE_URL = process.env.USER_SERVICE_BASE_URL || `http://localhost:${port}`;
 
 const server = http.createServer(index);
 
 await connectToDB()
   .then(() => {
     console.log("MongoDB Connected!");
-
     server.listen(port);
-    console.log("User service server listening on " + USER_SERVICE_BASE_URL);
+    console.log("User service server listening on port " + port);
   })
   .catch((err) => {
     console.error("Failed to connect to DB");
